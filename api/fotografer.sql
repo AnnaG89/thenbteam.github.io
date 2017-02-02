b3551df245da8374206584cd97a3a870092c41f0
tonilingardsson <toni.lingardsson@gmail.com>
2017-02-02 09:42:37 +0100

    Uppdaterad med Epost

--- api/fotografer.sql
+++ api/fotografer.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+    -- phpMyAdmin SQL Dump
 -- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 31, 2017 at 02:50 PM
+-- Generation Time: Feb 02, 2017 at 09:36 AM
 -- Server version: 5.6.33
 -- PHP Version: 7.0.12
 
@@ -11,7 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `Newbie`
+-- Database: `thenbteam`
 --
 
 -- --------------------------------------------------------
@@ -23,6 +23,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `Fotografer` (
   `Id` int(11) NOT NULL,
   `Fotograf` varchar(256) COLLATE utf8_swedish_ci NOT NULL,
+  `Epost` varchar(256) CHARACTER SET utf8 NOT NULL,
   `Insta` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Stad` varchar(256) COLLATE utf8_swedish_ci NOT NULL,
   `Webb` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -33,9 +34,10 @@ CREATE TABLE `Fotografer` (
 -- Dumping data for table `Fotografer`
 --
 
-INSERT INTO `Fotografer` (`Id`, `Fotograf`, `Insta`, `Stad`, `Webb`, `Lösen`) VALUES
-(1, 'Suzanna K', 'https://www.instagram.com/fotografsuzannak/', 'Staffanstorp', 'fotograf.suzannak.se', ''),
-(2, 'Maria Westerlind', 'https://www.instagram.com/mariawesterlind/', 'Sigtuna', 'www.mariawideman.se', '');
+INSERT INTO `Fotografer` (`Id`, `Fotograf`, `Epost`, `Insta`, `Stad`, `Webb`, `Lösen`) VALUES
+(1, 'Suzanna K', '', 'https://www.instagram.com/fotografsuzannak/', 'Staffanstorp', 'fotograf.suzannak.se', ''),
+(2, 'Maria Westerlind', '', 'https://www.instagram.com/mariawesterlind/', 'Sigtuna', 'www.mariawideman.se', ''),
+(3, '', 'myemail@domain.com', '', '', NULL, 'mypassword');
 
 --
 -- Indexes for dumped tables
@@ -55,4 +57,4 @@ ALTER TABLE `Fotografer`
 -- AUTO_INCREMENT for table `Fotografer`
 --
 ALTER TABLE `Fotografer`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
