dc454246f421e9f3a67d30a37a070fbf3f07efd9
TobiasPettersson <pettersson.tobias84@gmail.com>
2017-02-05 22:24:11 +0100

    Updates in photagraphers and about. Made profile modal

--- script/main.js
+++ script/main.js
@@ -1,1 +1,23 @@
+$(document).on('click', '#logInButton', function () {
+    var email;
+    var password;
+    alert("Clicked the log in button");
+});
 
+$(document).on('click', '#registerConfirmButton', function () {
+  var firstName;
+  var lastName;
+  var email;
+  var password1;
+  var password2;
+  var city;
+  var website;
+
+    alert("Clicked confirm registration");
+});
+
+$(document).on('click', '#recoverPasswordButton', function () {
+  var email;
+
+    alert("Clicked the recover password button");
+});
