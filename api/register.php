7fd45cabf6ed3c11fcf69b61c1342d42e7a6a7f3
Anna Gustafsson <annagustafsson@Annas-MacBook-Pro.local>
2017-02-07 08:37:31 +0100

    Uppdaterat crypt

--- api/register.php
+++ api/register.php
@@ -3,6 +3,10 @@ session_start();
 ?>
 <?php
 require "config.php";
+
+$passwd='fpassword2';
+$hash = crypt($passwd, "salt");
+
 if(isset($_POST['femail']) ){
     $sql="SELECT COUNT(*) AS 'antal_rader' FROM `Fotografer` WHERE `email`= :email";
     $stm_count = $pdo->prepare($sql);
@@ -10,9 +14,9 @@ if(isset($_POST['femail']) ){
     foreach($stm_count as $row){
         $antal_rader = $row['antal_rader'];
     }if($antal_rader == 0){
-        $sql = "INSERT INTO `Fotografer`(`firstname`, `lastname`, `company`, `email`, `insta`, `website`, `city`, `password`)VALUES(:firstname, :lastname, :company, :email, :insta, :website, :city, :password)";
+        $sql = "INSERT INTO `Fotografer`(`firstname`, `lastname`, `company`, `email`, `website`, `city`, `password`)VALUES(:firstname, :lastname, :company, :email, :website, :city, :password)";
         $stm_insert = $pdo->prepare($sql);
-        $stm_insert->execute(['firstname' => $_POST['ffirstname'], 'lastname' => $_POST['flastname'], 'company' => $_POST['fcompany'], 'email' => $_POST['femail'], 'insta' => $_POST['finsta'], 'website' => $_POST['fwebsite'], 'city' => $_POST['fcity'], 'password' => $_POST['fpassword']]);
+        $stm_insert->execute(['firstname' => $_POST['ffirstname'], 'lastname' => $_POST['flastname'], 'company' => $_POST['fcompany'], 'email' => $_POST['femail'], 'website' => $_POST['fwebsite'], 'city' => $_POST['fcity'], 'password' => $hash]);
         echo "<h1>OK</h1>";
     }else{
        echo "<h1>Denna emailadress är redan registrerad, logga in.</h1>"; 
