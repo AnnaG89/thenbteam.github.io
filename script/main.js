380dbea234618a3938d428a4dc477877122d6fdf
TobiasPettersson <pettersson.tobias84@gmail.com>
2017-02-07 15:37:18 +0100

    Registration in progress

--- script/main.js
+++ script/main.js
@@ -13,18 +13,19 @@ $(document).ready(function(){
         e.preventDefault();
         var firstName = $('.first_name').val();
         /*var lastName = $('.last_name').val();
-        var email = $('.company').val();
-        var password1 = $('.password1').val();
+        var comapny = $('.company').val();*/
+        var email = $('.email').val();
+        /*var password1 = $('.password1').val();
         var password2 = $('.password2').val();
         var city = $('.city').val();
         var website = $('.website').val();*/
 
         console.log(firstName);
-        alert("Clicked confirm registration " + firstName);
+        alert("Clicked confirm registration " + firstName + " " + email);
 
         //var url = "../api/register.php?";
 
-        $.post( "api/register.php?", { firstname: firstName} );
+        $.post( "api/register.php?", { firstname: firstName, email: email} );
 
 
     });
