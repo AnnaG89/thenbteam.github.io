49fdd5d3265430abc4f2342503b68e39bea4bc51
TobiasPettersson <pettersson.tobias84@gmail.com>
2017-02-08 10:08:14 +0100

    JS for register done

--- script/main.js
+++ script/main.js
@@ -1,51 +1,66 @@
-/*$(document).on('click', '#logInButton', function () {
-    var email;
-    var password;
-    alert("Clicked the log in button");
-});*/
 $(document).ready(function(){
-  /*function test(btn) {
-    var val = btn.form.querySelector('input[name="Montant"]').value;
-    alert(val);
-  }*/
 
+
+
+  // Registration form
   $("#registerWindow").on('click', '#registerConfirmButton', function (e) {
         e.preventDefault();
         var firstName = $('.first_name').val();
-        /*var lastName = $('.last_name').val();
-        var comapny = $('.company').val();*/
+        var lastName = $('.last_name').val();
+        var company = $('.company').val();
         var email = $('.email').val();
-        /*var password1 = $('.password1').val();
+        var password1 = $('.password1').val();
         var password2 = $('.password2').val();
         var city = $('.city').val();
-        var website = $('.website').val();*/
+        var website = $('.website').val();
 
-        console.log(firstName);
-        alert("Clicked confirm registration " + firstName + " " + email);
+        alert("Clicked confirm registration \n" +
+        "\nName: " + firstName + " " + lastName +
+        "\nCompany: " + company +
+        "\nEmail: " + email +
+        "\nPassword: " + password1 +
+        "\nPassword: " + password2 +
+        "\nCity: " + city +
+        "\nWebsite: " + website);
 
         //var url = "../api/register.php?";
 
-        //$.post( "../api/register.php?", { ffirstname: firstName, femail: email} );
-        $.post( "../api/register.php", $( ".registrationForm" ).serialize() );   
+        $.post( "../api/register.php?", { ffirstname: firstName, femail: email} ) //Todo: Ersätt med riktig url
+        .done(function(data){
+          console.log("success");
+        }
+        .fail(function(error){
+          console.log("Failed register");
+        });
+
 
     });
 
-
-    /*var firstName = $("#first_name").val();
-    var lastName;
-    var email;
-    var password1;
-    var password2;
-    var city;
-    var website;
-
-      console.log(firstName);
-  */
+    // Sign In form
+    $("#signInWindow").on('click', '#logInButton', function (e) {
+          e.preventDefault();
+          var email = $('.emailSignIn').val();
+          var password = $('.passwordSignIn').val();
 
 
-/*
-$(document).on('click', '#recoverPasswordButton', function () {
-  var email;
+          alert("Clicked sign in \n" +
+          "\nEmail: " + email +
+          "\nPassword: " + password);
 
-    alert("Clicked the recover password button");*/
+    });
+
+    // Recover Password
+    $("#recoverPasswordWindow").on('click', '#recoverPasswordButton', function (e) {
+          e.preventDefault();
+          var email = $('.emailRecover').val();
+
+
+          alert("Clicked recover button \n" +
+          "\nEmail: " + email);
+    });
+
+
+
+
+
 });
